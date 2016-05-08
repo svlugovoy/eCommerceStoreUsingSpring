87d38b997cb6a45492a68763a7d86fce6175c0bb
svlugovoy <svlugovoy@gmail.com>
2016-05-08 22:37:31 +0300

    do admin functionality

--- src/main/java/com/svlugovoy/estore/controller/HomeController.java
+++ src/main/java/com/svlugovoy/estore/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.svlugovoy.estore.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author Sergey Lugovoy <svlugovoy@gmail.com> 08.05.2016.
@@ -14,6 +16,22 @@ public class HomeController {
     public String home() {
 
         return "home";
+    }
+
+    @RequestMapping("/login")
+    public String login(@RequestParam(value = "error", required = false) String error,
+                        @RequestParam(value = "logout", required = false) String logout,
+                        Model model) {
+
+        if (error != null) {
+            model.addAttribute("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addAttribute("msg", "You have been logged out successfully.");
+        }
+
+        return "login";
     }
 
 }
---
+++ src/main/java/com/svlugovoy/estore/dao/impl/CartDaoImpl.java
@@ -0,0 +1,63 @@
+package com.svlugovoy.estore.dao.impl;
+
+import com.svlugovoy.estore.dao.CartDao;
+import com.svlugovoy.estore.model.Cart;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Sergey Lugovoy <svlugovoy@gmail.com> 05.05.2016.
+ */
+
+@Repository
+@Transactional
+public class CartDaoImpl implements CartDao {
+
+    private Map<String, Cart> listOfCarts;
+
+    public CartDaoImpl() {
+        listOfCarts = new HashMap<String, Cart>();
+    }
+
+    @Override
+    public Cart create(Cart cart) {
+
+        if (listOfCarts.keySet().contains(cart.getCartId())) {
+            throw new IllegalArgumentException(String.format(
+                    "Can not create a cart. A cart with given id(%) already exists.", cart.getCartId()));
+        }
+        listOfCarts.put(String.valueOf(cart.getCartId()), cart);
+
+        return cart;
+    }
+
+    @Override
+    public Cart read(String cartId) {
+
+        return listOfCarts.get(cartId);
+    }
+
+    @Override
+    public void update(String cartId, Cart cart) {
+
+        if (!listOfCarts.keySet().contains(cartId)){
+            throw new IllegalArgumentException(String.format(
+                    "Can not update cart. The cart with given id(%) does not exists.", cart.getCartId()));
+        }
+        listOfCarts.put(cartId, cart);
+    }
+
+    @Override
+    public void delete(String cartId) {
+
+        if (!listOfCarts.keySet().contains(cartId)){
+            throw new IllegalArgumentException(String.format(
+                    "Can not delete cart. The cart with given id(%) does not exists.", cartId));
+        }
+        listOfCarts.remove(cartId);
+    }
+
+}
