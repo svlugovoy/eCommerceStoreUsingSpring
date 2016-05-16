95972b5dd0142834af9be5fcccf0a6fecaf47bfd
svlugovoy <svlugovoy@gmail.com>
2016-05-16 15:09:19 +0300

    configuring spring web flow

--- src/main/java/com/svlugovoy/estore/dao/CartDao.java
+++ src/main/java/com/svlugovoy/estore/dao/CartDao.java
@@ -2,6 +2,8 @@ package com.svlugovoy.estore.dao;
 
 import com.svlugovoy.estore.model.Cart;
 
+import java.io.IOException;
+
 /**
  * @author Sergey Lugovoy <svlugovoy@gmail.com> 05.05.2016.
  */
@@ -10,6 +12,8 @@ import com.svlugovoy.estore.model.Cart;
 public interface CartDao {
 
     Cart getCartById(int cartId);
+
+    Cart validate(int cartId) throws IOException;
 
     void update(Cart cart);
 
--- src/main/java/com/svlugovoy/estore/dao/impl/CartDaoImpl.java
+++ src/main/java/com/svlugovoy/estore/dao/impl/CartDaoImpl.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+
 /**
  * @author Sergey Lugovoy <svlugovoy@gmail.com> 05.05.2016.
  */
@@ -25,6 +27,18 @@ public class CartDaoImpl implements CartDao {
         Session session = sessionFactory.getCurrentSession();
 
         return (Cart) session.get(Cart.class, cartId);
+    }
+
+    @Override
+    public Cart validate(int cartId) throws IOException {
+
+        Cart cart = getCartById(cartId);
+        if (cart == null || cart.getCartItems().size() == 0) {
+            throw new IOException(cartId + "");
+        }
+        update(cart);
+
+        return cart;
     }
 
     @Override
